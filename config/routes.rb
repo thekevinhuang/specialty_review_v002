312faa01f277acc12196647bf847e57cfc6ad30f
Kevin Huang <thekevinhuang@gmail.com>
2018-07-29 23:19:32 -0400

    added simple sessions logic

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   resources :users
+  
+  get '/signin' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  delete '/sessions' => 'sessions#destroy'
 end

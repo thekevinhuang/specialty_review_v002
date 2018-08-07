27cbced6dfd1426ead2df6f4799d2457631865bf
Kevin Huang <thekevinhuang@gmail.com>
2018-08-07 02:23:51 -0400

    added item model creation form

--- app/controllers/item_models_controller.rb
+++ app/controllers/item_models_controller.rb
@@ -1,3 +1,15 @@
 class ItemModelsController <ApplicationController
+    def show
 
+    end
+
+    def new
+        if params[:item_category_id]
+            @item_category = Activity.find_by(id: params[:item_category_id])
+        end
+    end
+
+    def create
+
+    end
 end

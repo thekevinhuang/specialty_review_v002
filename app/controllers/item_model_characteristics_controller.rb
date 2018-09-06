8a9918ca56aa293b725759255145b0e46330ff09
Kevin Huang <thekevinhuang@gmail.com>
2018-09-06 10:13:20 -0400

    added the addition of characteristics to item_models

--- app/controllers/item_model_characteristics_controller.rb
+++ app/controllers/item_model_characteristics_controller.rb
@@ -8,7 +8,25 @@ class ItemModelCharacteristicsController < ApplicationController
     end
 
     def create
+        item_model = ItemModel.find_by(id: params["item_model"]["id"])
+        if item_model
+            characteristic = Characteristic.find_or_initialize_by(name: params["characteristic"]["name"])
+            if !characteristic.id?
+                characteristic.description = params["characteristic"]["description"]
+            end
 
+            if characteristic.save
+                imchar = ItemModelCharacteristic.new(item_model_id: item_model.id, characteristic_id: characteristic.id)
+                imchar.save
+                redirect_to item_model_path(item_model)
+
+            else
+                redirect_to item_model_path(item_model)
+            end
+        else
+            redirect_to root_path
+        end
+        #binding.pry
     end
 
 end
